7ae28fc9ad183a8e8e19c0d9d2530aff7c7b71b5
Lavanda1 <144803499+Lavanda1@users.noreply.github.com>
2026-03-14 06:51:02 +0000

    add, добавили таблицу и кнопку

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+
+interface Book {
+  bookName: string
+  authorName: string
+  year: number
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,5 +17,13 @@ export class HomePage {
 
   constructor() {}
   myName: string = "София"
+  todo: string[] = [ "Помытся", "Убраться", "Вынести мусор"]
+  books: Book[] =[
+    {bookName:"Мертвые души", authorName:"Пушкин", year: 1980},
+    {bookName:"О моем перораждение", authorName:"Юкин", year: 2020},
+  ]
+  btnClick(){
+    this.myName = this.myName + "!"
+  }
 
 }
